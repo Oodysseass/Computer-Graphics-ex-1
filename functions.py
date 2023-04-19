06653ee124fba2d3330cc9642bf619eaff8d4ff1
Odysseas Sofikitis <o.sofikitis@gmail.com>
2023-04-19 17:29:27 +0300

    Fixes error in interpolation coefficient

--- functions.py
+++ functions.py
@@ -2,22 +2,24 @@ import numpy as np
 
 def interpolate_vectors(p1, p2, V1, V2, xy, dim):
     ## find line connecting p1-p2
-    m = (p2[1] - p1[1]) / (p2[0] - p1[0])
-    b = p1[1] - m * p1[0]
-
-    ## find missing coordinate of p
     # parallel to xx
     if p1[1] == p2[1]:
         # obviously dim == 1
         p = np.array([xy, p1[1]])
+
     # vertical line
     elif p1[0] == p2[0]:
         # obviously dim == 2
         p = np.array([p1[0], xy])
-    elif dim == 1:
-        p = np.array([xy, m * xy + b])
-    elif dim == 2:
-        p = np.array([(xy - b) / m, xy])
+
+    # normal line
+    else:
+        m = (p2[1] - p1[1]) / (p2[0] - p1[0])
+        b = p1[1] - m * p1[0]
+        if dim == 1:
+            p = np.array([xy, m * xy + b])
+        elif dim == 2:
+            p = np.array([(xy - b) / m, xy])
 
     ## find p-p1 and p1-p2 distance
     # p-p1
@@ -31,7 +33,7 @@ def interpolate_vectors(p1, p2, V1, V2, xy, dim):
     l = d1 / d2
 
     # interpolate
-    V = (1 - l) * V1 + l * V2
+    V = l * V1 + (1 - l) * V2
 
     return V
 
