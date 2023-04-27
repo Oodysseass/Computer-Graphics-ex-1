cf9e602cbd1f7ecbba46e368e7c2d8958ee4aa21
Odysseas Sofikitis <o.sofikitis@gmail.com>
2023-04-27 03:35:43 +0300

    Finalizes gouraud shading

--- functions.py
+++ functions.py
@@ -51,6 +51,8 @@ def interpolate_vectors(p1, p2, V1, V2, xy, dim):
 
 def flats(canvas, vertices, vcolors):
     updatedcanvas = canvas
+
+    # color that will be used
     flat_color = np.mean(vcolors, axis = 0)
 
     # one point
@@ -156,7 +158,7 @@ def Gourauds(canvas, vertices, vcolors):
     updatedcanvas = canvas
 
     if all(vertices[0] == vertices[1]) and all(vertices[1] == vertices[2]):
-        updatedcanvas[vertices[0, 0], vertices[0, 1]] = vcolors[1]
+        updatedcanvas[vertices[0, 0], vertices[0, 1]] = np.mean(vcolors, axis=0)
         return updatedcanvas
 
     edges = [Edge() for _ in range(3)]
@@ -170,7 +172,6 @@ def Gourauds(canvas, vertices, vcolors):
     y_min = min([edge.y_min[1] for edge in edges])
     y_max = max([edge.y_max[1] for edge in edges])
 
-    # active edges
     actives = 0
     for edge in edges:
         if edge.y_min[1] == y_min:
@@ -178,23 +179,15 @@ def Gourauds(canvas, vertices, vcolors):
             actives = actives + 1
     border_points = []
 
-    # plain lane
-    # hold twice the same edge
     if actives == 3:
         for edge in edges:
             if edge.m == float('-inf'):
                 edge.active = False
                 actives = actives - 1
 
-    # triangle with first edge parallel to x'x
-    # find edge with m = 0
-    # initialize next as border
-    # initialize other edges as actives
-    # start from y_min + 1
     if actives == 3:
         for i, edge in enumerate(edges):
             if edge.m == 0:
-                # draw first line
                 for x in range(edge.vertices[0][0], edge.vertices[1][0] + 1):
                     canvas[y_min, x] = interpolate_vectors(edge.vertices[0], edge.vertices[1],\
                                                            vcolors[i], vcolors[(i + 1) % 3], \
@@ -202,35 +195,31 @@ def Gourauds(canvas, vertices, vcolors):
                 actives = actives - 1
                 edge.active = False
             else:
-                # border points: xk, m, edge
                 border_points.append([edge.y_min[0] + 1 / edge.m, edge.m, i])
         y_min = y_min + 1
 
-    # normal cases
     if len(border_points) == 0:
         for i, edge in enumerate(edges):
             if edge.active:
                 border_points.append([edge.y_min[0], edge.m, i])
 
     for y in range(y_min, y_max + 1):
-        # sort points
         border_points = sorted(border_points, key=lambda x: x[0])
 
-        # find color in points
-        color_A = interpolate_vectors(edges[border_points[0][2]].vertices[0, :], \
-                                      edges[border_points[0][2]].vertices[1, :], \
+        # find color in border scanline points
+        color_A = interpolate_vectors(edges[border_points[0][2]].vertices[0], \
+                                      edges[border_points[0][2]].vertices[1], \
                                       vcolors[border_points[0][2]], \
                                       vcolors[(border_points[0][2] + 1) % 3], \
                                       y, 2)
-        color_B = interpolate_vectors(edges[border_points[1][2]].vertices[0, :], \
-                                      edges[border_points[1][2]].vertices[1, :], \
+        color_B = interpolate_vectors(edges[border_points[1][2]].vertices[0], \
+                                      edges[border_points[1][2]].vertices[1], \
                                       vcolors[border_points[1][2]], \
                                       vcolors[(border_points[1][2] + 1) % 3], \
                                       y, 2)
-
-        # draw between points
         for x in range(math.floor(border_points[0][0] + 0.5), \
                        math.floor(border_points[1][0] + 0.5) + 1):
+            # find color in scanline
             canvas[y, x] = interpolate_vectors(np.array([math.floor(border_points[0][0] + 0.5), y]), \
                                                np.array([math.floor(border_points[1][0] + 0.5) + 1, y]), \
                                                color_A, color_B, x, 1)
@@ -238,20 +227,20 @@ def Gourauds(canvas, vertices, vcolors):
         if y == y_max:
             break
 
-        # move previous points
         for point in border_points:
             point[0] = point[0] + 1 / point[1]
 
-        # add new edge
-        # and its point
         for i, edge in enumerate(edges):
             if edge.y_min[1] == y + 1:
                 edge.active = True
                 actives = actives + 1
                 border_points.append([edge.y_min[0], edge.m, i])
 
-        # remove if three
-        # and remove its border point
+        # only one edge case for m = 0 on last edge
+        if border_points[-1][1] == 0:
+            del border_points[-1]
+            continue
+
         if actives == 3:
             for i, edge in enumerate(edges):
                 if edge.y_max[1] == y + 1:
